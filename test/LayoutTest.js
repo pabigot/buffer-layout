b0fff1a5f901fa18f14cd2d9847401c17c2495f4
Peter A. Bigot <pab@pabigot.com>
2015-10-24 14:28:42 -0500

    UInt: implement little-endian unsigned int layouts

--- test/LayoutTest.js
+++ test/LayoutTest.js
@@ -13,7 +13,7 @@ suite("Layout", function () {
             var d = new lo.Layout(8, 'tag');
             assert(d instanceof lo.Layout);
             assert.equal(8, d.span);
-            assert.strictEqual('tag', d.property);
+            assert.equal('tag', d.property);
         });
         test("invalid ctor", function () {
             assert.throws(function () { new lo.Layout(); }, TypeError);
@@ -25,6 +25,50 @@ suite("Layout", function () {
                 b = new Buffer(3);
             assert.throws(function () { d.decode(b); });
             assert.throws(function () { d.encode('sth', b); });
+        });
+    });
+    suite("UInt", function () {
+        test("u8", function () {
+            var d = lo.u8('t'),
+                b = new Buffer(1);
+            assert(d instanceof lo.UInt);
+            assert(d instanceof lo.Layout);
+            assert.equal(1, d.span);
+            assert.equal('t', d.property);
+            b.fill(0);
+            assert.equal(0, d.decode(b));
+            d.encode(23, b);
+            assert.equal(0, Buffer('17', 'hex').compare(b));
+            assert.equal(23, d.decode(b));
+        });
+        test("u16", function () {
+            var d = lo.u16('t'),
+                b = new Buffer(2);
+            assert(d instanceof lo.UInt);
+            assert(d instanceof lo.Layout);
+            assert.equal(2, d.span);
+            assert.equal('t', d.property);
+            b.fill(0);
+            assert.equal(0, d.decode(b));
+            d.encode(0x1234, b);
+            assert.equal(0, Buffer('3412', 'hex').compare(b));
+            assert.equal(0x1234, d.decode(b));
+        });
+        test("u48", function () {
+            var d = lo.u48('t'),
+                b = new Buffer(6);
+            assert(d instanceof lo.UInt);
+            assert(d instanceof lo.Layout);
+            assert.equal(6, d.span);
+            assert.equal('t', d.property);
+            b.fill(0);
+            assert.equal(0, d.decode(b));
+            d.encode(0x123456789abc, b);
+            assert.equal(0, Buffer('bc9a78563412', 'hex').compare(b));
+            assert.equal(0x123456789abc, d.decode(b));
+        });
+        test("invalid ctor", function () {
+            assert.throws(function () { new lo.UInt(8); }, TypeError);
         });
     });
 });
